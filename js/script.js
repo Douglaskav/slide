8ef93ec2142f8d8d609b765ffbb359d384ef8298
Douglaskav <dnmsmpvp@gmail.com>
2019-04-13 11:38:29 -0300

    Custom Controls estilizado

--- js/script.js
+++ js/script.js
@@ -3,7 +3,4 @@ import SlideNav from './slide.js';
 const slide = new SlideNav('.slide', '.slide-wrapper');
 slide.init();
 slide.addArrow('.prev', '.next');
-
-slide.addControl();
-
-console.log(slide);
+slide.addControl('.custom-controls');
